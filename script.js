97980255be6fa14de3f279366227c3863bf7546f
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-03-15 16:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-//your JS code here. If required.
-let student={
-	name: "mina",
-};
-	funtion getKeys(){
-		let arr=[];
-	for(let i in student){
-		arr.push(i);
+// //your JS code here. If required.
+// let student={
+// 	name: "mina",
+// };
+// 	funtion getKeys(){
+// 		let arr=[];
+// 	for(let i in student){
+// 		arr.push(i);
 		
-	}
-	return arr;
-},
-obj.__proto__=getKeys();
-obj.getKeys();
+// 	}
+// 	return arr;
+// },
+// obj.__proto__=getKeys();
+// obj.getKeys();
+
+
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
