7949ecf3745a4c0e13a3b7d5eb154396016ad942
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2024-03-15 15:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let obj={
+	name: "mina",
+};
+	funtion getKeys(){
+		let arr=[];
+		let a=0;
+	for(let i in obj){
+		arr[a]=i;
+	}
+	return arr;
+},
+obj.__proto__=getKeys();
+obj.getKeys();
